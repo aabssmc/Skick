dc071417c16d631ee54c040117d3c41252e1c59f
aabssmc <lilmikeyninja203@gmail.com>
2023-11-26 22:04:03 -0800

    Added CondIsMod, CondIsOwner and CondIsStaff Conditions

---
+++ src/main/java/lol/aabss/skick/skript/conditions/CondIsStaff.java
@@ -0,0 +1,56 @@
+package lol.aabss.skick.skript.conditions;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Condition;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.util.Kleenean;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
+import uk.co.mistyknives.kick4j.api.chatrooms.payload.Chatter;
+
+import javax.annotation.Nullable;
+
+import static lol.aabss.skick.Skick.client;
+
+public class CondIsStaff extends Condition {
+
+    Expression<String> user;
+    Expression<String> channel;
+    boolean is;
+
+    static{
+        Skript.registerCondition(CondIsMod.class,
+                "%string% (is|are) [a] [channel] staff for %string%",
+                "%string% (is|are)( not|n't) [a] [channel] staff for %string%"
+        );
+    }
+
+    @Override
+    public boolean check(@NotNull Event e) {
+        Chatter chatter = client.chatrooms.cache.getChatter(channel.getSingle(e), user.getSingle(e));
+        if (is){
+            return chatter.isStaff();
+        }
+        return !chatter.isStaff();
+    }
+
+    @NotNull
+    @Override
+    public String toString(@Nullable Event e, boolean debug) {
+        return "user is owner";
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public boolean init(@NotNull Expression<?>[] exprs, int matchedPattern, @NotNull Kleenean isDelayed, @NotNull SkriptParser.ParseResult parseResult) {
+        if (client == null){
+            Skript.error("Kick isn't logged in. (see SecKickLogin)");
+            return false;
+        }
+        is = (matchedPattern == 0);
+        user = (Expression<String>) exprs[0];
+        channel = (Expression<String>) exprs[1];
+        return true;
+    }
+}
